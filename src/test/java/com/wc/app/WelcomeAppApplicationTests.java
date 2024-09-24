c538a6d7ff043899a9e6cb3f3f1e8e01594b4dd4
Spyron1234 <stack.surya@gmail.com>
2024-09-24 23:51:35 +0530

    Update WelcomeAppApplicationTests.java

--- src/test/java/com/wc/app/WelcomeAppApplicationTests.java
+++ src/test/java/com/wc/app/WelcomeAppApplicationTests.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,15 +15,16 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 class WelcomeAppApplicationTests {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(WelcomeAppApplicationTests.class);
 
-	 @Autowired
-	    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-	    @Test
-	    public void testHelloEndpoint() throws Exception {
-	        mockMvc.perform(get("/welcome/msg"))
-	                .andExpect(status().isOk())
-	                .andExpect(content().string("Welcome to git"));
-	    }
+	@Test
+	public void testHelloEndpoint() throws Exception {
+		LOGGER.info("Test Case run start for controller");
+		mockMvc.perform(get("/welcome/msg")).andExpect(status().isOk()).andExpect(content().string("Welcome to git"));
+	}
 
 }
